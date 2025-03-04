5426320abb1c4aee42538b3c0371bd387df3b41b
Arjun Moola <arjunmoola123@gmail.com>
2025-03-04 12:35:28 -0600

    changes

--- handlers.go
+++ handlers.go
@@ -4,6 +4,7 @@ import (
     "github.com/bwmarrin/discordgo"
 )
 
-func setRosterHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
+func setRosterHandler(bot *NbaFantasyBot) func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+    return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+    }
 }
