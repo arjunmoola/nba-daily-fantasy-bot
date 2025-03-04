9cff19d360260cf66e203fad68e6287e4de6038d
Arjun Moola <arjunmoola123@gmail.com>
2025-03-04 10:46:06 -0600

    changes

--- main.go
+++ main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+    "fmt"
+    "log"
+    "os"
     "github.com/bwmarrin/discordgo"
     "github.com/joho/godotenv"
 )
 
 type NbaFantasyBot struct {
-    client *NbaFantasyClient
+    //client *NbaFantasyClient
     session *discordgo.Session
 }
 
+func newNbaFantasyBot() (*NbaFantasyBot, error) {
+    token := os.Getenv("TOKEN")
+
+    if token == "" {
+        return nil, fmt.Errorf("discord token is not set")
+    }
+
+    session, err := discordgo.New("Bot " + token)
+
+    if err != nil {
+        return nil, err
+    }
+
+    bot := NbaFantasyBot{
+        session: session,
+    }
+
+    return &bot, nil
+}
+
+func (b *NbaFantasyBot) Open() error {
+    return b.session.Open()
+}
+
+func (b *NbaFantasyBot) Close() error {
+    return b.session.Close()
+}
+
 func main() {
+    if err := godotenv.Load(); err != nil {
+        log.Panic(err)
+    }
+
+    bot, err := newNbaFantasyBot()
+
+    if err != nil {
+        log.Panic(err)
+    }
+
+    if err := bot.Open(); err != nil {
+        log.Panic(err)
+    }
+
+    defer bot.Close()
+
+    fmt.Println("hello world")
 }
